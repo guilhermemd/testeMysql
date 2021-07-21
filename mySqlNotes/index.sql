ffffbcc658fcb60dd755131161e6e4a239a59a31
LucasMLara <lucasml@live.com>
2021-07-21 15:23:01 -0300

    testing select+concat

---
+++ mySqlNotes/index.sql
@@ -0,0 +1,12 @@
+SELECT 'Olá, bem-vindo ao SQL!';
+SELECT 10;
+SELECT now();
+SELECT 20 * 2;
+SELECT 50 / 2;
+SELECT 18 AS idade;
+SELECT 2019 AS ano;
+SELECT 'Rafael', 'Martins', 25, 'Desenvolvedor Web';
+SELECT 'Rafael' AS nome, 'Martins' AS sobrenome, 25 AS idade, 'Desenvolvedor Web' AS 'Área de atuação';
+SELECT CONCAT (first_name, last_name) FROM sakila.actor;
+SELECT CONCAT(first_name, ' ', last_name) AS 'Nome Completo' FROM sakila.actor;
+
